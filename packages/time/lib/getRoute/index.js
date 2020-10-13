436381182ea3a57d23581c886a90d51fe0fab017
omrilotan <omri@hamadgera.com>
2020-10-13 16:00:14 +0300

    time: support route set as array

--- packages/time/lib/getRoute/index.js
+++ packages/time/lib/getRoute/index.js
@@ -5,5 +5,15 @@
  */
 module.exports = ({baseUrl, route = {}} = {}) => [
 	baseUrl,
-	typeof route.path === 'string' ? route.path : '*',
+	getPath(route),
 ].filter(Boolean).join('/');
+
+/**
+ * Gets path route string or array
+ * @param  {Object} request path
+ * @return {String} Route path
+ */
+function getPath({ path } = {}) {
+	const value = Array.isArray(path) ? path[0] : path;
+	return typeof value === 'string' ? value : '*';
+}
